dbe377ef07fd85c3187bd13e9c8e9bb2fb3ac4e5
David <david@zete.es>
2020-09-08 20:08:16 +0200

    + Componentes básicos

--- udemy-curso/src/App.js
+++ udemy-curso/src/App.js
@@ -1,6 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+class Hello extends Component {
+  render() {
+    return <h2>{this.props.title}</h2>
+  }
+}
+
+class Text extends Component {
+  render() {
+
+    const textoSegunBool = this.props.isActivated ? 'On' : 'Off'
+
+    // Multiplico cada número del array por 2
+    const mappedNumbers = this.props.arrayOfNumbers.map(n=> n * 2)
+
+    return (
+      <div>
+        <p>{this.props.text}</p>
+        <p>{this.props.number}</p>
+        <p>{textoSegunBool}</p>
+
+        {/* El join hace que cada elemento del array se le ponga una coma */}
+        <p>{this.props.arrayOfNumbers.join(', ')}</p>
+        <p>{mappedNumbers.join(', ')}</p>
+      </div>
+    )
+
+  }
+}
 
 function App() {
   return (
@@ -8,17 +37,23 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Aplicación de React de ejemplo.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+
+
       </header>
+
+      <Hello title="Dentro en el componente"></Hello>
+
+      <Text
+        text='Texto del componente'
+        number={15}
+        isActivated={true}
+        arrayOfNumbers={[2, 3, 4]}
+
+      />
+
     </div>
   );
 }
